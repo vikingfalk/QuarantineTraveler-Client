c7a4df1ae2bc033f88b30be662816ff6096f7ff9
Viking Falk <vikingfalk@gmail.com>
2021-04-14 12:53:03 +0200

    improved flags responsiveness

--- src/styledElements/FlagCard.js
+++ src/styledElements/FlagCard.js
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components';
 export default styled.article`
   display: block;
   width: 100%;
+  height: calc(100vw * 0.25);
   overflow: hidden;
   box-shadow: 0 0 10px 1px rgba(0, 0, 0, 0.2);
   border-radius: 5px;
@@ -16,9 +17,13 @@ export default styled.article`
 
   @media (min-width: 480px) {
     &:hover {
-    ${props => !props.selected && css`
-      box-shadow: 0 0 6px 6px rgba(80, 255, 80, 0.6);
-    `}
-  }
+      ${props => !props.selected && css`
+        box-shadow: 0 0 6px 6px rgba(80, 255, 80, 0.6);
+      `}
+    }
+
+    & {
+      height: 100%;
+    }
   }
 `;
